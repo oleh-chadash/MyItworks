6be2e87ea33cc3ab19693709b6f270ff1897eedc
oleh-chadash <4talks@gmail.com>
2024-03-14 07:35:05 +0200

    js

---
+++ 3 week/JavaScript Codio/ex.js
@@ -0,0 +1,19 @@
+let myString = "Hello, world!";
+let myNumber = 42;
+let p2 = {
+	name: "Robert",
+	age: 25,
+	city: "Jerusalem"}
+let myHomeWork = true;
+let myTime = null;
+let myUndefined;
+let myUniqueSymbol = Symbol('my symbol');
+
+
+console.log(typeof myString);
+console.log(myNumber);
+console.log(p2);
+console.log(myHomeWork);
+console.log(myTime);
+console.log(myUndefined);
+console.log(myUniqueSymbol);
